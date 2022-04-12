00104c1b40dabc285ed79da1f00358a2ad77a738
Alexxboc <boccardi.alessandro@gmail.com>
2022-04-12 15:16:38 +0200

    Create function to generate posts in html

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,6 +74,73 @@ const posts = [
     }
 ]
 
+/* 
+Milestone 2
+Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+*/
+
+// Creo una funzione che generi il markup da inserire nell'html
+function generatePostMarkup(post_date,user_post_name, user_post_img, post_text, main_post_img) {
+    return `
+    <div class="post">
+                <div class="post_top">
+                    <div class="post_top_img">
+                        <img src="${user_post_img}" alt="">
+                    </div>
+                    <div class="user">
+                        <div class="user_name">${user_post_name}</div>
+                        <div class="date">${post_date}</div>
+                    </div>
+                </div>
+                <div class="main_text">
+                    <p>
+                        ${post_text}
+                    </p>
+                </div>
+                
+                <div class="post_img">
+                    <img src="${main_post_img}" alt="">
+                </div>
+
+                <div class="post_bottom">
+                    <div class="like_section">
+                        <a href="">
+                            <i class="fa-solid fa-thumbs-up"></i>
+                            <span>Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="like_views">
+                        Piace a <strong id="counter">80</strong> persone
+                    </div>
+                </div>
+            </div>
+    `
+}
+
+// Creo una funzione che inserisca il markup nell'html
+function generatePosts(array_object, dom_selector) {
+    // Seleziono l'elemento della Dom dove inserire l'html
+const postAreaElement = document.querySelector(dom_selector);
+// console.log(postAreaElement);
+// Ciclo l'array di oggetti
+array_object.forEach(post => {
+    // console.log(post);
+    // Genero il markup per ogni singolo post
+    const postMarkup = generatePostMarkup(post.date, post.user_name, post.user_img, post.text, post.main_img)
+    // console.log(postMarkup)
+    // Inserisco il markup nell'html
+    postAreaElement.insertAdjacentHTML("beforeend", postMarkup)
+})
+}
+
+generatePosts(posts, '.post_area')
+
+
+
+
+
+
+
 
 
     
