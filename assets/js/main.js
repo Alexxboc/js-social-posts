42343a5429a81a08c0cba8959fde2d1bc455989b
Alexxboc <boccardi.alessandro@gmail.com>
2022-04-12 14:43:06 +0200

    Create single post html structure and object array

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,14 +14,70 @@ numero di likes.
 
 const posts = [
     {
-        post_id: '',
-        post_number: '',
-        user_name: '',
-        user_img: '',
-        date: '',
-        text: '',
-        main_img: '',
-        likes: ''
+        post_id: 1,
+        user_name: 'Alessandro Boccardi',
+        user_img: 'https://picsum.photos/200',
+        date: '03/12/2022',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+        main_img: 'https://picsum.photos/800/400',
+        likes: '50'
+    },
 
+    {
+        post_id: 2,
+        user_name: 'Antonella Riccio',
+        user_img: 'https://picsum.photos/200',
+        date: '05/10/2022',
+        text: 'Viva la vida',
+        main_img: 'https://picsum.photos/800/400',
+        likes: '1200'
+    },
+
+    {
+        post_id: 3,
+        user_name: 'Santiago Boccardi',
+        user_img: 'https://picsum.photos/200',
+        date: '22/01/2022',
+        text: 'Mi piace super mario 👨🏻!',
+        main_img: 'https://picsum.photos/800/400',
+        likes: '150'
+    },
+
+    {
+        post_id: 4,
+        user_name: 'Santiago Boccardi',
+        user_img: 'https://picsum.photos/200',
+        date: '22/01/2022',
+        text: 'Oggi alla partita di calcio ho fatto goal e abbiamo vinto ⚽️',
+        main_img: '',
+        likes: '2150'
+    },
+
+    {
+        post_id: 5,
+        user_name: 'Giuliana Bianchi',
+        user_img: 'https://picsum.photos/200',
+        date: '22/01/2022',
+        text: '👵🏻 👵🏻 👵🏻',
+        main_img: '',
+        likes: '20'
+    },
+
+    {
+        post_id: 6,
+        user_name: 'Federico Boccardi',
+        user_img: 'https://picsum.photos/200',
+        date: '22/01/2022',
+        text: 'Buongiorno Kafèèèèèè ☕️ ???',
+        main_img: '',
+        likes: '1'
     }
 ]
+
+
+
+    
+    
+
+
+
