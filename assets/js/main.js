59b16244082129dbc8d15cef6049840ca3b44885
Alexxboc <boccardi.alessandro@gmail.com>
2022-04-12 13:57:56 +0200

    Create post section html structure

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+/* 
+Milestone 1
+Creiamo il nostro array di oggetti che rappresentano ciascun post. Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+id del post, numero progressivo da 1 a n
+nome autore,
+foto autore,
+data in formato americano (mm-gg-yyyy),
+testo del post,
+immagine (non tutti i post devono avere una immagine),
+numero di likes.
+*/
+
+// Creo un array di oggetti che rappresenti il post
+
+const posts = [
+    {
+        post_id: '',
+        post_number: '',
+        user_name: '',
+        user_img: '',
+        date: '',
+        text: '',
+        main_img: '',
+        likes: ''
+
+    }
+]
